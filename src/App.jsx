0a582a5103298824183403a2ae8a40df277e2a2d
Sudhanshu Singh <sudhanshusingh@Sudhanshus-MacBook-Air.local>
2025-06-19 23:27:18 +0530

    Initial React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import images from './images.png'
+function App() {
+ return( 
+ <div>
+  <h1>Lovely Professional University</h1>
+  <img src={images} alt="" />
+  <ul>
+    <li>Lovely Professional University empowers dreams.</li>
+    <li>A hub of innovation and excellence.</li>
+    <li>Shaping future leaders with knowledge.</li>
+  </ul>
+  <button>Submit</button>
+  </div>)
+}
+
+export default App
